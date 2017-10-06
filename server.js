58be6ff8456ff47563d407e40d7ac770bf55a474
soarflat <soarflat777@gmail.com>
2017-10-06 12:45:59 +0900

    [add] findOneAndUpdateについてのコメントを追加

--- server.js
+++ server.js
@@ -53,23 +53,40 @@ MongoClient.connect(`mongodb://${dbUser}:${dbPassword}@${dbName}`, (err, databas
   });
 
   app.put('/quotes', (req, res) => {
+    // findOneAndUpdateでdocumentを更新する
+    // https://docs.mongodb.com/v3.2/reference/method/db.collection.findOneAndUpdate/
+    // findOneAndUpdateはfilter（第二引数）とsort(第三引数)に基づいてdocumentを更新する
     db.collection('quotes')
-      .findOneAndUpdate({ name: req.body.name }, {
-        $set: {
-          name: req.body.name,
-          quote: req.body.quote,
-        }
-      }, {
-        sort: { _id: -1 },
-        upset: true,
-      }, (err, result) => {
-        if (err) {
-          return console.log(err)
-        }
+      .findOneAndUpdate(
+        // <filter>
+        // 更新の選択基準
+        // find（）メソッドと同じクエリセレクタが利用できる
+        { name: req.body.name },
 
-        console.log('updated to database');
-        res.redirect('/');
-      });
+        // <update>
+        // update operatorsを含める必要がある
+        // https://docs.mongodb.com/v3.2/reference/operator/update/
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote,
+          }
+        },
+
+        {
+          sort: { _id: -1 },
+          upset: true,
+        },
+
+        (err, result) => {
+          if (err) {
+            return console.log(err)
+          }
+
+          console.log('updated to database');
+          res.redirect('/');
+        }
+      );
   });
 
   app.delete('/quotes', (req, res) => {
